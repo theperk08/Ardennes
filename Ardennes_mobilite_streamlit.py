7db6b851e0f8ac933b342c328a728e592e942765
Manu <116085483+theperk08@users.noreply.github.com>
2023-05-25 17:51:42 +0200

    Update Ardennes_mobilite_streamlit.py

--- Ardennes_mobilite_streamlit.py
+++ Ardennes_mobilite_streamlit.py
@@ -99,7 +99,7 @@ def liste_proches(lat_depart, lon_depart, data, nombre, attribut):
         lon_2 = data.loc[index, 'lon']
         nom = data.loc[index, attribut]        
         
-        liste_distances = pd.concat([liste_distances, pd.DataFrame({'index': index, 'lat' : lat_2, 'lon' : lon_2, 'distance' : distance_simple(lat_depart, lon_depart, lat_2, lon_2), 'nom' : nom})])
+        liste_distances = pd.concat([liste_distances, pd.DataFrame.from_dict({'index': index, 'lat' : lat_2, 'lon' : lon_2, 'distance' : distance_simple(lat_depart, lon_depart, lat_2, lon_2), 'nom' : nom})])
               
     
     liste_distances = liste_distances.sort_values(by = 'distance').head(nombre)
