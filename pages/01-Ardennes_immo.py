3d31e6adfcf5fb73d7f5c5255caf0a2bffa71403
Manu <116085483+theperk08@users.noreply.github.com>
2023-05-31 12:05:32 +0200

    Update 01-Ardennes_immo.py

--- pages/01-Ardennes_immo.py
+++ pages/01-Ardennes_immo.py
@@ -4,13 +4,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# >>> import plotly.express as px
-# >>> fig = px.box(range(10))
-# >>> fig.write_html('test.html')
-
 st.header("Données du marché immobilier à Charleville-Mézières")
 
 HtmlFile = open("Immo_08.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
-#print(source_code)
-components.html(source_code, height=800)
+components.html(source_code, width = 1000, height = 800)
